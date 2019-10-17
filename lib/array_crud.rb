8a852e20a07e749a0e6cbc99c45ce62491c3074f
Yining Wang <yining.wang@utexas.edu>
2019-10-17 20:23:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  family = []
 end
 
 def create_an_array
-  
+  family = ['chaichai', 'gege', 'gaga', 'gagi']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
